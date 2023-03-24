51e9a10c791d4c05b83a4176adae79bdb60f8c6a
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-03-25 01:39:14 +0300

    axios taskAPI

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,23 +11,79 @@ const instance = axios.create ({
 })
 
 
-export const todolistsApi = {
+export const todolistApi = {
     getTodolists () {
         return instance.get<TodolistType[]>('todo-lists')
     },
 
-    createTodolists (title:string) {
+    createTodolist (title:string) {
         return instance.post<AxiosTodolistType<{ item:TodolistType }>>('todo-lists',{title})
     },
 
-    deleteTodolists (todolistId:string) {
+    deleteTodolist (todolistId:string) {
         return instance.delete<AxiosTodolistType>(`todo-lists/${todolistId}`)
     },
 
-    updateTodolists (todolistId:string,putAfterItemId:string) {
+    updateTodolist (todolistId:string,putAfterItemId:string) {
         return instance.put<AxiosTodolistType>(`todo-lists/${todolistId}`,{putAfterItemId})
     },
 }
+
+export const taskApi = {
+    getTasks (todolistId:string) {
+        return instance.get<AxiosGetTaskType>(`todo-lists/${todolistId}/tasks`)
+    },
+
+    createTask (todolistId:string,title:string) {
+        return instance.post<AxiosTaskType<{item:TaskType}>>(`todo-lists/${todolistId}/tasks`,{title})
+    },
+
+    deleteTask (todolistId:string,taskId:string) {
+        return instance.delete<AxiosTaskType>(`todo-lists/${todolistId}/tasks/${taskId}`)
+    },
+
+    updateTask (todolistId:string,taskId:string,payload:PayloadTaskType) {
+        return instance.put<AxiosTaskType<{item:TaskType}>>(`todo-lists/${todolistId}/tasks/${taskId}`,{payload})
+    }
+}
+
+type PayloadTaskType = {
+    title:string,
+    description:string,
+    completed:boolean,
+    status:number,
+    priority:number,
+    startDate:string,
+    deadline:string
+}
+
+type AxiosTaskType<T={}> = {
+    messages:string[]
+    resultCode:number
+    data:T
+}
+
+type AxiosGetTaskType = {
+    items:TaskType[]
+    totalCount:number
+    error:string
+}
+
+type TaskType = {
+    description:string,
+    title:string,
+    completed:boolean,
+    status:number,
+    priority:number,
+    startDate:string,
+    deadline:string,
+    id:string,
+    todoListId:string,
+    order:number,
+    addedDate:string
+}
+
+
 
 type AxiosTodolistType<T={}> = {
     resultCode:number,
